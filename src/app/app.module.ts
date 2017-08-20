3283fe047121a05150b5d20cd4a75e99751c8cca
Dharmanand <kumar.kd20@gmail.com>
2017-08-20 08:39:03 +0530

    add files..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { appRouting } from './app.routing';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpModule } from '@angular/http';
 import { AppComponent }  from './app.component';
+import { HomeComponent }  from './home/home.component';
+import { AboutComponent }  from './about/about.component';
+import { ContactComponent }  from './contact/contact.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, HttpModule, appRouting ],
+  declarations: [ AppComponent,
+                  HomeComponent,
+                  AboutComponent,
+                  ContactComponent
+                ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
